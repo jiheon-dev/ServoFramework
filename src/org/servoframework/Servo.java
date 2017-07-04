1ad3f45c0823fe47bef39de1fe30525578d96dfc
Smaker <admin@smaker.kr>
2017-07-04 19:32:28 +0900

    Rename Servo.class's variable

--- src/org/servoframework/Servo.java
+++ src/org/servoframework/Servo.java
@@ -4,7 +4,7 @@ package org.servoframework;
  * Created by unidev on 2017. 7. 4..
  */
 public class Servo {
-    public final static String versionName = "0.0.1";
+    public final static String VersionName = "0.0.1";
     private int port = 80;
     private static final String TAG = "ServoFramework";
 
