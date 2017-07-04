0133ca9c7a7ff68a6b3e5a0d87475fee5d10a207
Smaker <admin@smaker.kr>
2017-07-04 19:14:38 +0900

    Add Router.class annotation

---
+++ src/org/servoframework/annotation/Router.java
@@ -0,0 +1,18 @@
+package org.servoframework.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by unidev on 2017. 7. 4..
+ */
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Router {
+    enum RouteMethod {ALL, GET, POST, ETC}          // define METHOD
+
+    String route();                                 // where is ROUTED PATH
+    RouteMethod method() default RouteMethod.ALL;   // METHOD
+}
