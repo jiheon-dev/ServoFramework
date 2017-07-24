d0948188fffb0b7b51979af3aa422cd7b29054a5
Smaker <admin@smaker.kr>
2017-07-24 16:29:22 +0900

    Update SubRouter.java - return value

--- ServoTest/src/test/controllers/SubRouter.java
+++ ServoTest/src/test/controllers/SubRouter.java
@@ -1,8 +1,10 @@
 package test.controllers;
 
 import org.servoframework.annotation.Route;
+import org.servoframework.database.DBConnector;
 import org.servoframework.request.Request;
 import org.servoframework.response.Response;
+import test.models.User;
 
 /**
  * Created by unidev on 2017. 7. 4..
@@ -12,7 +14,8 @@ public class SubRouter {
     @Route(route="/sub", method = Route.RouteMethod.GET)
     public static void index(Request req, Response res) {
         res.setHeader("Content-Type", "text/html");
-        res.write("Hello Sub");
+        String result = User.findUser(req.getQuery("name"));
+        res.write(result);
         res.end();
     }
 }
