44ebf1c0688b85dd9a7a39ed4aa0cd97d4f61287
Smaker <admin@smaker.kr>
2017-07-24 15:58:35 +0900

    Add User.init() method

--- ServoTest/src/test/MainHandler.java
+++ ServoTest/src/test/MainHandler.java
@@ -3,18 +3,19 @@ package test;
 import org.servoframework.Servo;
 import test.controllers.MainRouter;
 import test.controllers.SubRouter;
+import test.models.User;
 
 /**
  * Created by unidev on 2017. 7. 4..
  */
 
 public class MainHandler {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws InterruptedException {
         Servo servo = new Servo();
 
         servo.addController(MainRouter.class);
         servo.addController(SubRouter.class);
-        
-        servo.startServer(8080);
+        User.init();
+        servo.startServer(7000);
     }
 }
