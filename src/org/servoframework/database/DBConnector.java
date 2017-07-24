1acfc63e1831609ce2dbf2208e581dea58274301
Smaker <admin@smaker.kr>
2017-07-24 15:03:20 +0900

    Update DBConnector.java

--- src/org/servoframework/database/DBConnector.java
+++ src/org/servoframework/database/DBConnector.java
@@ -1,7 +1,10 @@
 package org.servoframework.database;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 /**
  * Created by unidev on 2017. 7. 24..
@@ -28,4 +31,25 @@ public class DBConnector {
 
     MongoClient mongoClient = new MongoClient(host, port);
     MongoDatabase database = mongoClient.getDatabase(dbname);
+
+    public FindIterable<Document> findQuery(String collection, Document docs) {
+        MongoCollection<Document> coll = database.getCollection(collection);
+        return coll.find(docs);
+    }
+
+    public void insertQuery(String collection, Document docs) {
+        MongoCollection<Document> coll = database.getCollection(collection);
+        coll.insertOne(docs);
+    }
+
+    public void deleteQuery(String collection, Document docs) {
+        MongoCollection<Document> coll = database.getCollection(collection);
+        coll.deleteOne(docs);
+    }
+
+    public void updateQuery(String collection, Document docs, Document changeDocs) {
+        MongoCollection<Document> coll = database.getCollection(collection);
+        coll.updateOne(docs, changeDocs);
+    }
+
 }
