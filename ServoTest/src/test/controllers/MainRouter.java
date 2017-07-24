c0ac99d195873a75f7111cb8494d74748f793dd1
Smaker <admin@smaker.kr>
2017-07-24 12:40:29 +0900

    Update MainRouter.java - cookie test & write log

--- ServoTest/src/test/controllers/MainRouter.java
+++ ServoTest/src/test/controllers/MainRouter.java
@@ -3,12 +3,15 @@ package test.controllers;
 import org.servoframework.annotation.Route;
 import org.servoframework.request.Request;
 import org.servoframework.response.Response;
+import org.servoframework.util.Log;
 
 /**
  * Created by unidev on 2017. 7. 4..
  */
 
 public class MainRouter {
+    private static final String TAG = "ServoFramework";
+
     @Route(route="/", method = Route.RouteMethod.GET)
     public static void index(Request req, Response res) {
         res.setHeader("Content-Type", "text/html");
@@ -17,11 +20,12 @@ public class MainRouter {
     }
 
 
-    @Route(route="/home", method = Route.RouteMethod.GET)
+    @Route(route="/home", method = Route.RouteMethod.POST)
     public static void home(Request req, Response res) {
-        res.setHeader("Content-Type", "text/html");
+        res.setHeader("Content-Type", "application/x-www-form-urlencoded");
         res.cookie("key", "home");
         res.write("Hello Home");
+        Log.i(TAG, req.getQuery("name"));
         res.end();
     }
 }
