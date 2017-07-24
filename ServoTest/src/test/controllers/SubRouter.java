90c6b6a57c47b4018ffe3bbc267fff83b2583ed7
Smaker <admin@smaker.kr>
2017-07-24 17:19:00 +0900

    Update SubRouter.java

--- ServoTest/src/test/controllers/SubRouter.java
+++ ServoTest/src/test/controllers/SubRouter.java
@@ -2,8 +2,10 @@ package test.controllers;
 
 import org.servoframework.annotation.Route;
 import org.servoframework.database.DBConnector;
+import org.servoframework.http.SpecificRouting;
 import org.servoframework.request.Request;
 import org.servoframework.response.Response;
+import org.servoframework.util.URLDecode;
 import test.models.User;
 
 /**
@@ -14,8 +16,16 @@ public class SubRouter {
     @Route(route="/sub", method = Route.RouteMethod.GET)
     public static void index(Request req, Response res) {
         res.setHeader("Content-Type", "text/html");
-        String result = User.findUser(req.getQuery("name"));
+        User user = new User();
+        user.init();
+
+        System.out.println(req.getQuery("name"));
+        String result = user.findUser(URLDecode.getResult(req.getQuery("name")));
+
+        System.out.println(result);
+
         res.write(result);
+        User.close();
         res.end();
     }
 }
